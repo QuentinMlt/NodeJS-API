a701766a859923e8258e11d41a6cdcaee65c59b4
QuentinMlt <quentinmaillotpro@gmail.com>
2022-05-23 16:06:31 +0200

    import de joi donc test pour insert ok

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+require('express-async-errors');
+const app = express(); 
 
-const app = express() 
+const Joi = require("joi");
 
 const Collection = require("./Tache.js");
 const Taches = new Collection("Taches");
+
 
 app.use(express.json());
 
@@ -15,5 +18,19 @@ app.get("/api/taches", (req, res) => {
     res.json(Taches.getAll());
   });
 
+  app.post('/api/taches/add', (req, res) => {
+    const payload = req.body;
+    const joiSchema = Joi.object({
+        description: Joi.string().max(255).required(),
+        faite: Joi.boolean().required()
+    });
+    const {value, error} = joiSchema.validate(payload);
+    if (error) {
+        throw new Error(error.details[0].message);
+    }
+    Taches.insertOne(value);
+    res.status(201).send(value);
+});
+  
 
-app.listen(5000);
+  module.exports = app;
